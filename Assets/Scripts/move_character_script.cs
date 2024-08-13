5d72078c8648eb969c6627b177701ff7a1fd29c6
paivalucasss <lucaspaiva2805@gmail.com>
2024-08-13 18:55:38 -0300

    Add character movement for both sides

--- Assets/Scripts/move_character_script.cs
+++ Assets/Scripts/move_character_script.cs
@@ -8,6 +8,7 @@ public class move_character_script : MonoBehaviour
     private float horizontal;
     public float moveSpeed;
     private bool facingRight = true;
+    public Animator animator;
 
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private Rigidbody2D body;
@@ -19,20 +20,22 @@ public class move_character_script : MonoBehaviour
         float xinput = Input.GetAxis("Horizontal");
         //float yinput = Input.GetAxis("y");
         print(xinput * moveSpeed);
-        Vector2 moveSideways = new Vector2(xinput * moveSpeed * Time.deltaTime, body.velocity.y).normalized;
+        Vector2 moveSideways = new Vector2(xinput * moveSpeed * Time.deltaTime, body.velocity.y);
         Vector2 stop = new Vector2(0, 0);
 
         if (xinput > 0)
         {
+            animator.SetFloat("Speed", moveSideways.x);
             body.velocity = moveSideways;
-
         }
         else if (xinput < 0)
         {
+            animator.SetFloat("Speed", moveSideways.x);
             body.velocity = moveSideways;
         }
         else if (xinput == 0)
         {
+            animator.SetFloat("Speed", moveSideways.x);
             body.velocity = stop;
         }
     }
